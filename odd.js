0e10588fd555717ee988dc75d64db1efe9a53261
Asir <mdasir017@gmaol.com>
2025-08-07 01:26:50 +0600

    added the last update

--- odd.js
+++ odd.js
@@ -1,11 +1,38 @@
 
 
-// let num = 0 ;
-// while(num < 5){
-//     console.log(num);
-//     num++;
+// let num = 0 ;     //loop variable 
+// while(num < 5){    //loop condition
+//     console.log(num); //loop statement
+//     num++;    // loop variable changes
 // }
 
-for( let num = 0 ; num < 5 ; num++){
-    console.log(num);
+// for( let num = 0 ; num < 5 ; num++){
+//     console.log(num);
+// }
+
+
+
+// even number
+
+// for( let number = 0; number <= 20 ; number++){
+//     console.log(number);
+// }
+
+// even num
+// for( let num = 0 ; num <= 20 ; num = num + 2){
+//     console.log(num);
+// }
+
+// let num = 0 ;
+// while( num <= 15){
+//     console.log(num);
+//     num = num + 2;
+// }
+
+
+// odd number
+
+let num = 0 ;
+for( let i = 1 ; i <= 15 ; i =i+2){
+    console.log(i);
 }
