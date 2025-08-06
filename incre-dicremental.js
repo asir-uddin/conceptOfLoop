b8c417365bdd5871ce563f985ed35625267d418a
Asir <mdasir017@gmaol.com>
2025-08-07 03:01:56 +0600

    added last update

---
+++ incre-dicremental.js
@@ -0,0 +1,19 @@
+
+
+//incremental for loop
+// for(let i = 0; i <= 10 ; i++){
+//     console.log(i);
+// }
+
+
+//dicremental for loop
+
+// for( let i = 10; i >= 0; i--){
+//     console.log(i);
+// }
+
+let num = 20;
+while(num >= 10){
+    console.log(num);
+    num--;
+}
