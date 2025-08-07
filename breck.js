54f642e5ee6fa95650347fa88f515d71e11286ae
Asir <mdasir017@gmaol.com>
2025-08-07 23:54:35 +0600

    added the last commit

---
+++ breck.js
@@ -0,0 +1,15 @@
+
+
+// for( let i = 0; i < 30 ; i++){
+//     console.log(i);
+//     if( i >= 10){
+//         break;
+//     }
+// }
+// console.log( 'life after break up')
+
+let n = 54 ;
+while( n > 50){
+    console.log(n);
+    n-- ;
+}
---
+++ different-loop.js
@@ -0,0 +1,63 @@
+
+
+//Odd Number
+// for( let num = 0 ; num <= 20; num++){
+//     if( num % 2 === 1){
+//         console.log(num);
+//     }
+// }
+
+// for(let i = 0; i <= 20; i++){
+//     if(i % 2 !== 0){
+//         console.log(i);
+//     }
+// }
+// for( let i = 1 ; i <= 20 ; i += 2){
+//     console.log(i);
+// }
+
+// let i = 0;
+// while( i <= 30){
+//     if( i % 2 === 1){
+//        console.log(i); 
+//     }
+//     i++;
+// }
+
+
+// give me the list numbers between 1 to 30 divisible by 5
+// for( let i = 1; i <= 30 ; i++){
+//     if( i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for( let i = 1 ; i <= 30 ; i++){
+//     if( i % 5 === 0 || i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for( let i = 1 ; i<= 30 ; i++){
+//     if( i % 3 === 0 && i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+// for( let i = 1 ; i<= 100 ; i++){
+//     if( i % 3 === 0 && i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+//give me sum of the numbers between 1 to 30 divisible by 3
+// let total = 0 ;
+// for( let i = 1 ; i <= 30; i++){
+//     if( i % 3 === 0){
+//         console.log(i);
+//         total = total + i ;
+//         console.log( 'total', total);
+
+//     }
+// }
+// console.log( 'total sum of the number' , total);
